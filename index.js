f72b00d9a83297e0e7b905898735c92b97afa555
Abhishek Rajeshirke <abhishek.raj@media.net>
2019-10-16 17:39:58 +0530

    Changed path of dist folder for common js

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-module.exports = require('./src/curlirize');
+const ES6 = require('./src/curlirize');
+const { default: CommonJS } = require('./dist/curlirize');
+module.exports = typeof process === 'object' ? CommonJS : ES6;
